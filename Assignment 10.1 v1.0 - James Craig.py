5856cb8cff48eaf0ce0d72ebdd40f45404a9bf94
King-James-II <105425391+King-James-II@users.noreply.github.com>
2022-05-22 12:07:52 -0400

    Added requirement 3 to assignment
    Now searches for directory entered by the user and creates it if it didn't exist.

--- Assignment 10.1 v1.0 - James Craig.py
+++ Assignment 10.1 v1.0 - James Craig.py
@@ -1,2 +1,9 @@
-directory = input("In what directory do you wish to save your file?")
+import os
+directory = input("In what directory do you wish to save your file?\n c:/")
+file_path = f"/{directory}"
 file_name = input("File name: ")
+exists = os.path.exists(file_path)
+if exists == False:
+    os.makedirs(file_path)
+else:
+    pass
